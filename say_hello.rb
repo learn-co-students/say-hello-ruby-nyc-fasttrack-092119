e5917968f6a75e5ec821bcfd8dbefb83a32244b4
Isaiah Velez <isaiah.velez611@gmail.com>
2019-10-27 06:44:30 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,14 @@
 # Build your say_hello method here
 
-name = "isaiah"
-def = say_hello
+##
+##def say_hello()
 
-def say_hello(name)
-  puts "say hello!, #{name}"
+
+def say_hello(name = "Kent Beck")
+  if name == "Kent Beck" 
+      puts "Hello #{name}!"
+  elsif name == 
+      puts "Hello Ruby Programmer!"
 end
+
+
